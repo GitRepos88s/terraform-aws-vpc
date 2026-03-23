43bd3242a140c68f8200d70de0a3475dc52a3adc
Arunram Rajasekaran <ramarun04@gmail.com>
2026-03-23 11:26:26 +0530

    initial add

---
+++ peering.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc_peering_connection" "foo" {
+  #peer_owner_id = var.peer_owner_id
+
+  #Accepter
+  peer_vpc_id   = data.aws_vpc.default.id
+
+  #Requester
+  vpc_id        = aws_vpc.main.id
+
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = ${var.project}-${var.environment}-default
+    }
+  )
+}
