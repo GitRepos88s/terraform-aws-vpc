a9731a0b9317d4ff0c684e49f653bd1e17fc8bfb
Arunram Rajasekaran <ramarun04@gmail.com>
2026-03-25 11:37:37 +0530

    initial add

--- peering.tf
+++ peering.tf
@@ -1,4 +1,5 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
   #peer_owner_id = var.peer_owner_id
 
   #Accepter
@@ -23,4 +24,11 @@ resource "aws_vpc_peering_connection" "foo" {
         Name = "${var.project}-${var.environment}-default"
     }
   )
+}
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
 }
